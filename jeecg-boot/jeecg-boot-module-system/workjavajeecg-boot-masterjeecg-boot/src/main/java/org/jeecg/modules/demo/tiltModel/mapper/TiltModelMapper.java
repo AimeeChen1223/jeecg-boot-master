31a81d9592cd671cf3d37df63931b70701ea5b4f
chenmeng <aimeemchen@sina.com>
2019-07-11 10:48:02 +0800

    运用代码生成器构建倾斜模型数据表

---
+++ jeecg-boot/jeecg-boot-module-system/workjavajeecg-boot-masterjeecg-boot/src/main/java/org/jeecg/modules/demo/tiltModel/mapper/TiltModelMapper.java
@@ -0,0 +1,17 @@
+package org.jeecg.modules.demo.tiltModel.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+import org.jeecg.modules.demo.tiltModel.entity.TiltModel;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * @Description: 倾斜模型数据表
+ * @Author: jeecg-boot
+ * @Date:   2019-07-11
+ * @Version: V1.0
+ */
+public interface TiltModelMapper extends BaseMapper<TiltModel> {
+
+}
---
+++ jeecg-boot/jeecg-boot-module-system/workjavajeecg-boot-masterjeecg-boot/src/main/java/org/jeecg/modules/demo/tiltModel/service/ITiltModelService.java
@@ -0,0 +1,14 @@
+package org.jeecg.modules.demo.tiltModel.service;
+
+import org.jeecg.modules.demo.tiltModel.entity.TiltModel;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * @Description: 倾斜模型数据表
+ * @Author: jeecg-boot
+ * @Date:   2019-07-11
+ * @Version: V1.0
+ */
+public interface ITiltModelService extends IService<TiltModel> {
+
+}
